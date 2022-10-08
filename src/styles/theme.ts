e8ea11e0b0cd4d201cb6695c480011c2d45e4a16
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-08 00:06:54 -0300

    feat(theme): define as cores dos  stylos

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,27 @@
 import { extendTheme } from '@chakra-ui/react';
 
-export const theme = extendTheme({  
-    fonts:{
-        heading: 'Poppins',
-        body: 'Poppins',
+export const theme = extendTheme({
+  colors: {
+    white: {
+      "50": "#F5F8FA"
+    },
+    gray: {
+      "700": "#47585B"
+    },
+    yellow: {
+      "700": "#FFBA08"
     }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'white-50',
+        color: 'gray.700'
+      }
+    }
+  }
 })
